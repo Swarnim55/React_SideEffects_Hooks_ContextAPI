c0948ecef4da855776327caa5ce3bd8a21bb2a5e
Swarnim Paudyal <swarnimpaudyal55@gmail.com>
2023-02-25 15:32:17 +0545

    Used context API and useContext Hook to manage state appwide

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useEffect, useState } from 'react';
+
+import Login from './components/Login/Login';
+import Home from './components/Home/Home';
+import MainHeader from './components/MainHeader/MainHeader';
+import AuthContext from './components/context/auth-context';
+
+function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    const logInfo = localStorage.getItem('isLoggedIn');
+
+    if (logInfo === '1') {
+      setIsLoggedIn(true);
+    }
+  }, []);
+
+  const loginHandler = (email, password) => {
+    // We should of course check email and password
+    // But it's just a dummy/ demo anyways
+    setIsLoggedIn(true);
+    localStorage.setItem('isLoggedIn', '1');
+  };
+
+  const logoutHandler = () => {
+    localStorage.removeItem('isLoggedIn');
+    setIsLoggedIn(false);
+  };
+
+  return (
+    <>
+      <AuthContext.Provider
+        value={{
+          isLoggedIn: isLoggedIn,
+        }}
+      >
+        <MainHeader onLogout={logoutHandler} />
+        <main>
+          {!isLoggedIn && <Login onLogin={loginHandler} />}
+          {isLoggedIn && <Home onLogout={logoutHandler} />}
+        </main>
+      </AuthContext.Provider>
+    </>
+  );
+}
+
+export default App;
